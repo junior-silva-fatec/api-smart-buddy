ff6050d6ff5cd127c68bc30f770aa7b352aaa0a8
junior-silva-fatec <161388620+junior-silva-fatec@users.noreply.github.com>
2024-05-07 15:24:10 -0300

    rota users

--- app.js
+++ app.js
@@ -7,11 +7,30 @@ var http = require("http");
 const app = express();
 app.use(express.json());
 
-var server = http.createServer(function (request, response) {
+/* var server = http.createServer(function (request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
   response.end("Hello World Node!\n");
 });
 
 const porta = process.env.PORT;
 server.listen(porta);
-console.log(`Servidor iniciado em http://localhost:${porta}`);
+console.log(`Servidor iniciado em http://localhost:${porta}`); */
+
+const userRouter = require("./routes/userRoutes");
+app.use("/users", userRouter);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Erro de conexão ao MongoDB"));
+db.once("open", () => {
+  console.log("Conectado ao MongoDB Atlas!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
